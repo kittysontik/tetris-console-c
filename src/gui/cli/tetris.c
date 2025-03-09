29ee5825fda7a93f2930d27aa0be3d78a278d8ec
Jigsaw Lott <jigsawlo@student.21-school.ru>
2025-03-09 18:07:44 +0700

    init field

--- src/gui/cli/tetris.c
+++ src/gui/cli/tetris.c
@@ -1,15 +1,40 @@
-#include <ncurses.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-int main()
+
+#include "tetris.h"
+
+int main(int argc, char *argv[])
 {
-    // int argc, char**argv
+    GameInfo_t gameInfo;
 
     initscr();
-    printw("Hello");
+
+    alloc_memory_field(&gameInfo);
+    print_field(&gameInfo);
+
     refresh();
     getch();
     endwin();
+
     return 0;
+}
+
+void alloc_memory_field(GameInfo_t *gameInfo)
+{
+    gameInfo->field = (int **)calloc(20, sizeof(int *));
+
+    for (int i = 0; i < 20; i++)
+    {
+        gameInfo->field[i] = (int *)calloc(10, sizeof(int));
+    }
+}
+
+void print_field(GameInfo_t *gameInfo)
+{
+    for (int i = 0; i < 20; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printw("%d", gameInfo->field[i][j]);
+        }
+        printw("\n");
+    }
 }
---
+++ src/gui/cli/tetris.h
@@ -0,0 +1,33 @@
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef enum
+{
+    Start,
+    Pause,
+    Terminate,
+    Left,
+    Right,
+    Up,
+    Down,
+    Action
+} UserAction_t;
+
+typedef struct
+{
+    int **field;
+    int **next;
+    int score;
+    int high_score;
+    int level;
+    int speed;
+    int pause;
+} GameInfo_t;
+
+// void userInput(UserAction_t action, bool hold);
+
+// GameInfo_t updateCurrentState();
+void alloc_memory_field(GameInfo_t *gameInfo);
+void print_field(GameInfo_t *gameInfo);
