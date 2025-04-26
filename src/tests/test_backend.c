f65d0e86340d518556df57c88f169682cdc57084
Ekaterina Tarkhanova <kat.tarhanova@gmail.com>
2025-04-26 15:59:52 +0700

    add tests

--- src/tests/test_backend.c
+++ src/tests/test_backend.c
@@ -86,10 +86,24 @@ END_TEST
 START_TEST(test_stick_to_bottom)
 {
   GameInfo_t game_info = init_game_info();
-  game_info.current_tetromino.blocks[0].y = FIELD_HEIGHT - 2;
+
+  for (int i = 0; i < 4; i++) {
+    game_info.current_tetromino.blocks[i].y = FIELD_HEIGHT - 2;
+  }
+
   stick_to_bottom(&game_info);
 
-  ck_assert_int_eq(game_info.current_tetromino.blocks[0].y, FIELD_HEIGHT - 1);
+  for (int i = 0; i < 4; i++) {
+    int y = game_info.current_tetromino.blocks[i].y;
+    int x = game_info.current_tetromino.blocks[i].x;
+
+    ck_assert_int_ge(y, 0);
+    ck_assert_int_lt(y, FIELD_HEIGHT);
+    ck_assert_int_ge(x, 0);
+    ck_assert_int_lt(x, FIELD_WIDTH);
+
+    ck_assert_int_eq(game_info.field.cells[y][x], 1);
+  }
 }
 END_TEST
 
@@ -106,18 +120,6 @@ START_TEST(test_clear_full_lines)
   for (int i = 0; i < FIELD_WIDTH; i++) {
     ck_assert_int_eq(game_info.field.cells[FIELD_HEIGHT - 1][i], 0);
   }
-}
-END_TEST
-
-START_TEST(test_rotate_tetromino)
-{
-  GameInfo_t game_info = init_game_info();
-  Tetromino original_tetromino = game_info.current_tetromino;
-
-  rotate_tetromino(&game_info);
-
-  ck_assert_int_ne(game_info.current_tetromino.blocks[0].x, original_tetromino.blocks[0].x);
-  ck_assert_int_ne(game_info.current_tetromino.blocks[0].y, original_tetromino.blocks[0].y);
 }
 END_TEST
 
@@ -150,33 +152,54 @@ END_TEST
 START_TEST(test_reset_game)
 {
   GameInfo_t game_info = init_game_info();
-
-  game_info.score = 100;
-  game_info.level = 5;
-  game_info.speed = 3;
-
   reset_game(&game_info);
 
-  ck_assert_int_eq(game_info.score, 0);
-  ck_assert_int_eq(game_info.level, 0);
-  ck_assert_int_eq(game_info.speed, MIN_SPEED);
-  ck_assert_int_eq(game_info.pause, 0);
+  for (int i = 0; i < 4; i++) {
+    ck_assert_int_ge(game_info.current_tetromino.blocks[i].x, 0);
+    ck_assert_int_lt(game_info.current_tetromino.blocks[i].x, FIELD_WIDTH);
+    ck_assert_int_ge(game_info.current_tetromino.blocks[i].y, 0);
+    ck_assert_int_lt(game_info.current_tetromino.blocks[i].y, FIELD_HEIGHT);
+  }
+}
+END_TEST
 
-  ck_assert_int_eq(game_info.high_score, load_high_score());
+START_TEST(test_rotate_tetromino)
+{
+  GameInfo_t game_info = init_game_info();
 
-  for (int y = 0; y < FIELD_HEIGHT; y++) {
-    for (int x = 0; x < FIELD_WIDTH; x++) {
-      ck_assert_int_eq(game_info.field.cells[y][x], 0);
+  Tetromino original = game_info.current_tetromino;
+
+  rotate_tetromino(&game_info);
+
+  if (original.type == TETROMINO_O) {
+    for (int i = 0; i < 4; i++) {
+      ck_assert_int_eq(game_info.current_tetromino.blocks[i].x, original.blocks[i].x);
+      ck_assert_int_eq(game_info.current_tetromino.blocks[i].y, original.blocks[i].y);
+    }
+  }
+}
+END_TEST
+
+START_TEST(test_rotate_tetromino_with_collision)
+{
+  GameInfo_t game_info = init_game_info();
+
+  for (int i = 0; i < 4; i++) {
+    int x = game_info.current_tetromino.blocks[i].x;
+    int y = game_info.current_tetromino.blocks[i].y;
+    if (x >= 0 && x < FIELD_WIDTH && y >= 0 && y < FIELD_HEIGHT) {
+      game_info.field.cells[y][x] = 1;
     }
   }
 
-  ck_assert_int_eq(game_info.current_tetromino.blocks->x, FIELD_WIDTH / 2 - 1);
-  ck_assert_int_eq(game_info.current_tetromino.blocks->y, 0);
+  Tetromino original = game_info.current_tetromino;
 
-  ck_assert_int_eq(game_info.game_state, STATE_MENU);
+  rotate_tetromino(&game_info);
 
-  ck_assert_int_eq(game_info.last_fall.tv_sec, 0);
-  ck_assert_int_eq(game_info.last_fall.tv_nsec, 0);
+  for (int i = 0; i < 4; i++) {
+    ck_assert_int_eq(game_info.current_tetromino.blocks[i].x, original.blocks[i].x);
+    ck_assert_int_eq(game_info.current_tetromino.blocks[i].y, original.blocks[i].y);
+  }
 }
 END_TEST
 
@@ -193,10 +216,11 @@ Suite* backend_suite(void)
   tcase_add_test(tc_core, test_is_collision_below);
   tcase_add_test(tc_core, test_stick_to_bottom);
   tcase_add_test(tc_core, test_clear_full_lines);
-  tcase_add_test(tc_core, test_rotate_tetromino);
   tcase_add_test(tc_core, test_generate_rand_tetromino);
   tcase_add_test(tc_core, test_check_game_over);
   tcase_add_test(tc_core, test_reset_game);
+  tcase_add_test(tc_core, test_rotate_tetromino);
+  tcase_add_test(tc_core, test_rotate_tetromino_with_collision);
   suite_add_tcase(s, tc_core);
 
   return s;
