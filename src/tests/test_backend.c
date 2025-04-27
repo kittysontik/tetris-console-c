4fcf9a2c239a380909567d9a51349d39678e3e38
Ekaterina Tarkhanova <kat.tarhanova@gmail.com>
2025-04-27 14:45:07 +0700

    add tests

--- src/tests/test_backend.c
+++ src/tests/test_backend.c
@@ -399,6 +399,285 @@ START_TEST(test_update_speed_limits_to_max_speed)
 }
 END_TEST
 
+START_TEST(test_handle_user_input_terminate)
+{
+  GameInfo_t game_info = {.game_state = STATE_PLAYING};
+  handle_user_input(Terminate, &game_info, NULL);
+  ck_assert_int_eq(game_info.game_state, STATE_EXIT);
+}
+END_TEST
+
+START_TEST(test_handle_user_input_pause_start)
+{
+  GameInfo_t game_info = {.pause = false};
+  handle_user_input(Pause, &game_info, NULL);
+  ck_assert_int_eq(game_info.pause, true);
+  handle_user_input(Start, &game_info, NULL);
+  ck_assert_int_eq(game_info.pause, false);
+}
+END_TEST
+
+START_TEST(test_handle_user_input_movement_when_paused)
+{
+  GameInfo_t game_info = {.pause = true};
+  handle_user_input(Down, &game_info, NULL);
+  handle_user_input(Left, &game_info, NULL);
+  handle_user_input(Right, &game_info, NULL);
+  handle_user_input(Action, &game_info, NULL);
+}
+END_TEST
+
+START_TEST(test_handle_input_if_any_no_action)
+{
+  GameInfo_t game_info = {0};
+  GameWindows windows = {0};
+  windows.game_win = newwin(10, 10, 0, 0);
+  wtimeout(windows.game_win, 1);
+  wgetch(windows.game_win);
+  handle_input_if_any(&game_info, &windows);
+  delwin(windows.game_win);
+}
+END_TEST
+
+START_TEST(test_handle_state_playing)
+{
+  GameInfo_t game_info = {.game_state = STATE_PLAYING};
+  GameWindows windows = {0};
+  handle_state_playing(&game_info, &windows);
+  ck_assert_int_eq(game_info.game_state, STATE_PLAYING);
+}
+END_TEST
+
+START_TEST(test_run_game_fsm_exit)
+{
+  GameInfo_t game_info = {.game_state = STATE_EXIT};
+  GameWindows windows = {0};
+  run_game_fsm(&game_info, &windows);
+  ck_assert_int_eq(game_info.game_state, STATE_EXIT);
+}
+END_TEST
+START_TEST(test_check_game_over_true)
+{
+  GameInfo_t game_info = {.game_state = STATE_PLAYING};
+
+  game_info.field.cells[0][0] = 1;
+
+  game_info.current_tetromino.blocks[0].x = 0;
+  game_info.current_tetromino.blocks[0].y = 0;
+
+  for (int i = 1; i < 4; i++) {
+    game_info.current_tetromino.blocks[i].x = -1;
+    game_info.current_tetromino.blocks[i].y = -1;
+  }
+
+  check_game_over(&game_info);
+
+  ck_assert_int_eq(game_info.game_state, STATE_GAME_OVER);
+}
+END_TEST
+
+START_TEST(test_check_game_over_false)
+{
+  GameInfo_t game_info = {.game_state = STATE_PLAYING};
+
+  memset(game_info.field.cells, 0, sizeof(game_info.field.cells));
+
+  for (int i = 0; i < 4; i++) {
+    game_info.current_tetromino.blocks[i].x = i;
+    game_info.current_tetromino.blocks[i].y = 0;
+  }
+
+  check_game_over(&game_info);
+
+  ck_assert_int_eq(game_info.game_state, STATE_PLAYING);
+}
+END_TEST
+
+START_TEST(test_handle_state_game_over)
+{
+  GameInfo_t game_info = {.game_state = STATE_GAME_OVER};
+  GameWindows windows = {0};
+  handle_state_game_over(&game_info, &windows);
+  ck_assert_int_eq(game_info.game_state, STATE_EXIT);
+}
+END_TEST
+
+START_TEST(test_map_key_to_action)
+{
+  ck_assert_int_eq(map_key_to_action('q'), Terminate);
+  ck_assert_int_eq(map_key_to_action('p'), Pause);
+  ck_assert_int_eq(map_key_to_action('s'), Start);
+  ck_assert_int_eq(map_key_to_action(KEY_LEFT), Left);
+  ck_assert_int_eq(map_key_to_action(KEY_RIGHT), Right);
+  ck_assert_int_eq(map_key_to_action(KEY_DOWN), Down);
+  ck_assert_int_eq(map_key_to_action(KEY_UP), Action);
+  ck_assert_int_eq(map_key_to_action('x'), NoAction);
+}
+END_TEST
+
+START_TEST(test_map_key_to_action_terminate)
+{
+  ck_assert_int_eq(map_key_to_action('q'), Terminate);
+}
+END_TEST
+
+START_TEST(test_map_key_to_action_pause)
+{
+  ck_assert_int_eq(map_key_to_action('p'), Pause);
+}
+END_TEST
+
+START_TEST(test_map_key_to_action_start)
+{
+  ck_assert_int_eq(map_key_to_action('s'), Start);
+}
+END_TEST
+
+START_TEST(test_map_key_to_action_left)
+{
+  ck_assert_int_eq(map_key_to_action(KEY_LEFT), Left);
+}
+END_TEST
+
+START_TEST(test_map_key_to_action_right)
+{
+  ck_assert_int_eq(map_key_to_action(KEY_RIGHT), Right);
+}
+END_TEST
+
+START_TEST(test_map_key_to_action_down)
+{
+  ck_assert_int_eq(map_key_to_action(KEY_DOWN), Down);
+}
+END_TEST
+
+START_TEST(test_map_key_to_action_action)
+{
+  ck_assert_int_eq(map_key_to_action(KEY_UP), Action);
+}
+END_TEST
+
+START_TEST(test_map_key_to_action_no_action)
+{
+  ck_assert_int_eq(map_key_to_action('x'), NoAction);
+}
+END_TEST
+
+START_TEST(test_handle_user_input_terminate_action)
+{
+  GameInfo_t game_info = {.game_state = STATE_MENU};
+  GameWindows game_windows;
+
+  handle_user_input(Terminate, &game_info, &game_windows);
+
+  ck_assert_int_eq(game_info.game_state, STATE_EXIT);
+}
+END_TEST
+
+START_TEST(test_handle_user_input_pause_action)
+{
+  GameInfo_t game_info = {.pause = false, .game_state = STATE_MENU};
+  GameWindows game_windows;
+
+  handle_user_input(Pause, &game_info, &game_windows);
+
+  ck_assert_int_eq(game_info.pause, true);
+}
+END_TEST
+
+START_TEST(test_handle_user_input_start_action)
+{
+  GameInfo_t game_info = {.pause = false, .game_state = STATE_MENU};
+  GameWindows game_windows;
+
+  handle_user_input(Start, &game_info, &game_windows);
+
+  ck_assert_int_eq(game_info.pause, true);
+}
+END_TEST
+
+START_TEST(test_handle_user_input_down_action_when_paused)
+{
+  GameInfo_t game_info = {.pause = true, .game_state = STATE_MENU};
+  GameWindows game_windows;
+
+  handle_user_input(Down, &game_info, &game_windows);
+
+  ck_assert_int_eq(game_info.game_state, STATE_MENU);
+}
+END_TEST
+
+START_TEST(test_handle_user_input_right_action_when_active)
+{
+  GameInfo_t game_info = {.pause = false, .game_state = STATE_MENU};
+  GameWindows game_windows;
+
+  handle_user_input(Right, &game_info, &game_windows);
+
+  ck_assert_int_eq(game_info.game_state, STATE_MENU);
+}
+END_TEST
+
+START_TEST(test_handle_user_input_left_action_when_active)
+{
+  GameInfo_t game_info = {.pause = false, .game_state = STATE_MENU};
+  GameWindows game_windows;
+
+  handle_user_input(Left, &game_info, &game_windows);
+
+  ck_assert_int_eq(game_info.game_state, STATE_MENU);
+}
+END_TEST
+
+START_TEST(test_handle_user_input_rotate_action_when_active)
+{
+  GameInfo_t game_info = {.pause = false, .game_state = STATE_MENU};
+  GameWindows game_windows;
+
+  handle_user_input(Action, &game_info, &game_windows);
+
+  ck_assert_int_eq(game_info.game_state, STATE_MENU);
+}
+END_TEST
+
+START_TEST(test_handle_user_input_unknown_key_action)
+{
+  GameInfo_t game_info = {.game_state = STATE_MENU};
+  GameWindows game_windows;
+
+  handle_user_input(99, &game_info, &game_windows);
+  ck_assert_int_eq(game_info.game_state, STATE_MENU);
+}
+END_TEST
+
+START_TEST(test_handle_full_lines_no_full_lines)
+{
+  GameInfo_t game_info;
+  memset(&game_info, 0, sizeof(game_info));
+
+  handle_full_lines(&game_info);
+
+  ck_assert_int_eq(has_full_rows(&game_info.field), 0);
+}
+END_TEST
+
+START_TEST(test_handle_full_lines_multiple_lines_partial)
+{
+  GameInfo_t game_info;
+  memset(&game_info, 0, sizeof(game_info));
+
+  for (int i = 0; i < FIELD_WIDTH; i++) {
+    game_info.field.cells[18][i] = 1;
+  }
+
+  handle_full_lines(&game_info);
+
+  for (int i = 0; i < FIELD_WIDTH; i++) {
+    ck_assert_int_eq(game_info.field.cells[19][i], 0);
+  }
+}
+END_TEST
+
 Suite* backend_suite(void)
 {
   Suite* s = suite_create("Backend");
@@ -435,6 +714,35 @@ Suite* backend_suite(void)
   tcase_add_test(tc_core, test_update_speed_basic);
   tcase_add_test(tc_core, test_update_speed_after_levelup);
   tcase_add_test(tc_core, test_update_speed_limits_to_max_speed);
+  tcase_add_test(tc_core, test_handle_user_input_terminate);
+  tcase_add_test(tc_core, test_handle_user_input_pause_start);
+  tcase_add_test(tc_core, test_handle_user_input_movement_when_paused);
+  tcase_add_test(tc_core, test_handle_input_if_any_no_action);
+  tcase_add_test(tc_core, test_handle_state_playing);
+  tcase_add_test(tc_core, test_run_game_fsm_exit);
+  tcase_add_test(tc_core, test_check_game_over_true);
+  tcase_add_test(tc_core, test_check_game_over_false);
+  tcase_add_test(tc_core, test_handle_state_game_over);
+  tcase_add_test(tc_core, test_map_key_to_action);
+  tcase_add_test(tc_core, test_map_key_to_action_terminate);
+  tcase_add_test(tc_core, test_map_key_to_action_pause);
+  tcase_add_test(tc_core, test_map_key_to_action_start);
+  tcase_add_test(tc_core, test_map_key_to_action_left);
+  tcase_add_test(tc_core, test_map_key_to_action_right);
+  tcase_add_test(tc_core, test_map_key_to_action_down);
+  tcase_add_test(tc_core, test_map_key_to_action_action);
+  tcase_add_test(tc_core, test_map_key_to_action_no_action);
+  tcase_add_test(tc_core, test_handle_user_input_terminate_action);
+  tcase_add_test(tc_core, test_handle_user_input_pause_action);
+  tcase_add_test(tc_core, test_handle_user_input_start_action);
+  tcase_add_test(tc_core, test_handle_user_input_down_action_when_paused);
+  tcase_add_test(tc_core, test_handle_user_input_right_action_when_active);
+  tcase_add_test(tc_core, test_handle_user_input_left_action_when_active);
+  tcase_add_test(tc_core, test_handle_user_input_rotate_action_when_active);
+  tcase_add_test(tc_core, test_handle_user_input_unknown_key_action);
+  tcase_add_test(tc_core, test_handle_full_lines_no_full_lines);
+  tcase_add_test(tc_core, test_handle_full_lines_multiple_lines_partial);
+
   suite_add_tcase(s, tc_core);
 
   return s;
